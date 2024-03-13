76272a3d5594ef75ef87db1b7aa7f14002d07815
mohammadjalizi <mjalizi1376@gmail.com>
2024-03-13 09:17:32 -0400

    Add  react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import { useState } from 'react'
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from './theme';
 import './App.css'
-import Header1 from 'components/header/Header1'
-import Header2 from 'components/header/Header2'
-import Header3 from 'components/header/Header3'
+import Header1 from './components/header/Header1'
+import Header2 from './components/header/Header2'
+import Header3 from './components/header/Header3'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [theme, colorMode] = useMode();
   return (
   
-<>
+    <ColorModeContext.Provider value={colorMode}>
 
-<Header1/>
+   <ThemeProvider theme={theme}><
+      <CssBaseline />
+    
+      <Header1/>
 <Header2/>
 <Header3/>
 
+    
+    /ThemeProvider>
 
 
 
 
-</>
+
+</ThemeProvider>
+    </ColorModeContext.Provider>
 
 
   )
--- src/components/header/Header1.jsx
+++ src/components/header/Header1.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import   { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+import { IconButton, useTheme } from "@mui/material";
+
+
 
 const Header1 = () => {
+  const colorMode = useContext(ColorModeContext);
+  const theme = useTheme();
   return (
-    <div>Header1</div>
-  )
-}
+    <div>
+      {theme.palette.mode === "light" ? (
+        <IconButton
+          onClick={() => {
+            localStorage.setItem(
+              "mode",
+              theme.palette.mode === "dark" ? "light" : "dark"
+            );
+            colorMode.toggleColorMode();
+          }}
+          color="inherit"
+        >
+          <LightModeOutlined />
+        </IconButton>
+      ) : (
+        <IconButton
+          onClick={() => {
+            localStorage.setItem(
+              "mode",
+              theme.palette.mode === "dark" ? "light" : "dark"
+            );
+            colorMode.toggleColorMode();
+          }}
+          color="inherit"
+        >
+          <DarkModeOutlinedIcon />
+        </IconButton>
+      )}
+    </div>
+  );
+};
 
-export default Header1
+export default Header1;
