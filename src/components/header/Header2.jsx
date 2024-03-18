ab8099303bdefbcfa4f4c21ac7cd8f35c0dc615f
mohammadjalizi <mjalizi1376@gmail.com>
2024-03-18 17:06:16 +0330

    Add   Navbar

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Header2 = () => {
   return (
-    <div>Header2</div>
+    <div className=' '>
+      
+<div className=' float-right mt-5 '>
+
+  <img src="src/assets/coffee_assets/espresso/portrait/espresso_pic_3_portrait.png" alt="" />
+</div>
+      
+    </div>
   )
 }
 
