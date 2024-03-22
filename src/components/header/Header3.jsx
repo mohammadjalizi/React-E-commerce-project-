fdac56dfd7cb6cf82a3400d51d4d94a972645f3e
mohammadjalizi <mjalizi1376@gmail.com>
2024-03-22 18:03:07 +0330

    Add   menu bar

--- src/components/header/Header3.jsx
+++ src/components/header/Header3.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import './slider.css'
 const Header3 = () => {
   return (
-    <div>
+    <div className=' flex justify-between items-center mx-20 '>
       
-<div className=' my-12 text-center text-3xl shadow-2xl  rounded-xl border w-96  m-auto  p-6'>
+< div  className='  ' > 
 
-<h1> محصولات پرفروش</h1>
 
+<h2 className=" my-6  text-3xl">ساخت قهوه اختصاصی و سفارشی</h2>
+
+اگر یه قهوه‌باز واقعی هستی میدونی چه میزان کافئین و چه نوع عطر و طعم لازم داری. پس دست به کار شو و قهوه خودتو بساز!
+<p>به راحتی با انتخاب دستگاه قهوه ساز، نوع قهوه و مقدارش،&nbsp; قهوه اختصاصی خودت رو بساز</p>
 </div>
+
+<div className='photo2'>
+<img className=' ' src="src/components/header/photo/jonathan-borba-vk2Xwf23rOQ-unsplash.jpg" alt="" />
+</div>
+
 
     </div>
   )
