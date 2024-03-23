86cf6d0403f272838315688e2bbde3a106c81dd3
mohammadjalizi <mjalizi1376@gmail.com>
2024-03-23 18:13:16 +0330

    Add   header3

--- src/components/header/Header3.jsx
+++ src/components/header/Header3.jsx
@@ -2,23 +2,66 @@ import React from 'react'
 import './slider.css'
 const Header3 = () => {
   return (
-    <div className=' flex  flex-wrap justify-between items-center mx-20 '>
-      
-< div  className='  ' > 
+<>
+<div className=' flex  mx-96 gap-11 my-9 '>
 
 
-<h2 className=" my-6  text-3xl">ساخت قهوه اختصاصی و سفارشی</h2>
-
-اگر یه قهوه‌باز واقعی هستی میدونی چه میزان کافئین و چه نوع عطر و طعم لازم داری. پس دست به کار شو و قهوه خودتو بساز!
-<p>به راحتی با انتخاب دستگاه قهوه ساز، نوع قهوه و مقدارش،&nbsp; قهوه اختصاصی خودت رو بساز</p>
+<div>
+  <img  
+    decoding="async"
+  
+    src="https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1.jpg"
+    className=" w-96 h-96  hover:cursor-pointer  hover:scale-95 "
+    alt="خرید دمنوش"
+    data-lazy-srcset="https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1-150x169.jpg 150w"
+    data-lazy-sizes="(max-width: 600px) 100vw, 600px"
+    data-lazy-src="https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1.jpg"
+    data-ll-status="loaded"
+    sizes="(max-width: 600px) 100vw, 600px"
+    srcSet="https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/1-1-1-150x169.jpg 150w"
+  />
+  
 </div>
 
-<div className='photo2'>
-<img className=' ' src="src/components/header/photo/jonathan-borba-vk2Xwf23rOQ-unsplash.jpg" alt="" />
+
+
+<div>
+
+<img
+  decoding="async"
+
+  src="https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2.jpg"
+  className=" w-96 h-96 "
+  alt="خرید چای سیاه"
+  data-lazy-srcset="https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2-150x169.jpg 150w"
+  data-lazy-sizes="(max-width: 600px) 100vw, 600px"
+  data-lazy-src="https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2.jpg"
+  data-ll-status="loaded"
+  sizes="(max-width: 600px) 100vw, 600px"
+  srcSet="https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/2-2-2-150x169.jpg 150w"
+/>
+
+</div>
+<div>
+
+<img
+  decoding="async"
+ 
+  src="https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3.jpg"
+  className=" w-96 h-96 hover:cursor-pointer hover:scale-95"
+  alt="خرید جعبه هدیه مهرگیاه"
+  data-lazy-srcset="https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3-150x169.jpg 150w"
+  data-lazy-sizes="(max-width: 600px) 100vw, 600px"
+  data-lazy-src="https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3.jpg"
+  data-ll-status="loaded"
+  sizes="(max-width: 600px) 100vw, 600px"
+  srcSet="https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3.jpg 600w, https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3-267x300.jpg 267w, https://mehr-e-giah.com/wp-content/uploads/2023/01/3-3-3-150x169.jpg 150w"
+/>
+
+</div>
 </div>
 
-
-    </div>
+</>
   )
 }
 
